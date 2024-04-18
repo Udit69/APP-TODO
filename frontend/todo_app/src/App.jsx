269409df29da26a9263111ae6dfca9009bb023d0
Udit bhadauria <UDITBHADAURIA.23041058@gehu.ac.in>
2024-04-19 00:06:14 +0530

    CREaTED NAVBAR HOME FOOTER AND ABOUT PAGE

--- frontend/todo_app/src/App.jsx
+++ frontend/todo_app/src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/navbar';
+import { Home } from './Components/Home/Home';
+import  Footer  from './Components/Footer/Footer';
+import About from './Components/about/About';
 
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/todo' element={<About/>}></Route>
+          <Route path='/signup' element={<About/>}></Route>
+          <Route path='/signin' element={<About/>}></Route>
+        </Routes>
+        
+      </Router>
+      <Footer/>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/todo_app/src/Components/Navbar/navbar.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import { MdLibraryBooks } from "react-icons/md";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          <b style={{ display: "flex", alignItems: "center" }}>
+            <MdLibraryBooks style={{ marginRight: "5px" }} />
+            TODO
+          </b>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item" style={{ marginRight: "10px" }}>
+              <Link className="nav-link" to="/">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item" style={{ marginRight: "10px" }}>
+              <Link className="nav-link" to="/about">
+                About us <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item" style={{ marginRight: "10px" }}>
+              <Link className="nav-link" to="/todo">
+                Todo <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item" style={{ marginRight: "10px" }}>
+              <Link className="nav-link btn-nav" to="/signup">
+                Sign up <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item" style={{ marginRight: "10px" }}>
+              <Link className="nav-link btn-nav" to="/signin">
+                Sign in <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item" style={{ marginRight: "10px" }}>
+              <Link className="nav-link btn-nav" to="#">
+                Logout <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                <img
+                  className="user-png"
+                  src="src\assets\icons8-user-100.png"
+                  alt="User Icon"
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
+
